a791eb789dbf22ce2bf69149bb747f457fa25fd6
Davfool888 <davfool888@gmail.com>
2025-09-17 17:01:02 -0500

    HandleChange inicio

--- src/Task.js
+++ src/Task.js
@@ -3,9 +3,7 @@ import './task.css'
 export function TaskCard({ready}) {
     return <div className='card'>
         <h1>Mi primer tarea </h1>
-        <span
-        style={ready ? {background:'#4f9c3aff', color:'#000000'}: {background:'#341fc2ff', color:'#fff'}}
-        >
+        <span className={ready ? 'bg_red': 'bg_green'}>
  {ready ? 'Tarea Realizada':'Tarea Pendiente'}
         </span>
     </div>
