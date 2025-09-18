a60a6b6be1659bab4be4a60688f43da35d529e86
Davfool888 <davfool888@gmail.com>
2025-09-18 11:28:25 -0500

    Hooks useState(2)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Posts.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
-  const [counter, setCounter] = useState(10)
+  const [counter, setCounter] = useState(10);
   return (
     <div>
       <h1>Counter:{counter} </h1>
-      <button onClick={() =>{
-        return setCounter(counter + 10)}}>Increment</button>
+      <button
+        onClick={() => {
+          setCounter(counter + 10);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 10);
+        }}
+      >
+        Descrement
+      </button>
+      <button
+        onClick={() => {
+          setCounter(0);
+        }}
+      >
+        Reiniciar
+      </button>
     </div>
   );
 }
 // Hola mundo
-root.render(<>
-<Counter/>
-</>);
+root.render(
+  <>
+    <Counter />
+  </>
+);
