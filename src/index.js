3bb7a665c12ce624c94cc0b09ac38a68b7b78d01
Davfool888 <davfool888@gmail.com>
2025-09-18 09:09:10 -0500

    Fetch api (Traer datos con api a mi app)

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
 import { Button } from "./button.js";
-import {TaskCard} from "./Task.js"
-import {Saludar} from './Saludar.js'
+import { TaskCard } from "./Task.js";
+import { Saludar } from "./Saludar.js";
+import { Posts } from "./Posts.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const handleChange= (e) =>{console.log(e.target.value)}
-
 // Hola mundo
-root.render(
-  <>
-  <TaskCard ready ={true}/>
-  <Saludar/>
-  <Button name="Click"/>
-  <input id="Hola" onChange={handleChange}/>
-  </>
-);
+root.render(<>
+<Posts/>
+</>);
