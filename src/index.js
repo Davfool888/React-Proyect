470da6cd8f14b884dd8ae9fb466513a89d48ac2e
Davfool888 <davfool888@gmail.com>
2025-09-18 11:20:23 -0500

    Hooks, useEstate(Primera parte)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Posts.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    name: "Ryan ray ",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Cain Reales ",
-    image: "https://robohash.org/user3",
-  },
-  {
-    id: 3,
-    name: "Aleja Vivas ",
-    image: "https://robohash.org/user4",
-  },
-];
-
+function Counter() {
+  const [counter, setCounter] = useState(10)
+  return (
+    <div>
+      <h1>Counter:{counter} </h1>
+      <button onClick={() =>{
+        return setCounter(counter + 10)}}>Increment</button>
+    </div>
+  );
+}
 // Hola mundo
-root.render(
-  <>
-    {users.map((user, index) => {
-      return <div key= {index}>
-        <h1 >{user.name}  {index}</h1>
-        <img src={user.image}></img>
-      </div>;
-    })}
-  </>
-);
+root.render(<>
+<Counter/>
+</>);
