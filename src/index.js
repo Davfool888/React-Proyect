2949134c32d9a350fb8656cd74e233d1adf68263
Davfool888 <davfool888@gmail.com>
2025-09-18 16:07:48 -0500

    Hooks useEffect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Posts.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
-  const [mensaje, setMensaje] = useState('');
+  const [mensaje, setMensaje] = useState("");
+  const [Counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("Render");
+  }, [Counter]);
+
   return (
     <div>
-      <input onChange={e => setMensaje(e.target.value)}></input>
-      <button onClick={() =>{
-        alert("El mensaje es: "+ mensaje)
-      }}>save</button>
+      <input onChange={(e) => setMensaje(e.target.value)}></input>
+      <button
+        onClick={() => {
+          alert("El mensaje es: " + mensaje);
+        }}
+      >
+        save
+      </button>
+      <hr />
+      <h1>Counter: {Counter} </h1>
+      <button onClick={() => setCounter(Counter + 1)}>Incrementar</button>
     </div>
   );
 }
