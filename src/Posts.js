8e909408b87cec2d495dc6c523c8f9db2f4b893b
Davfool888 <davfool888@gmail.com>
2025-09-18 10:55:28 -0500

    Arreglos en react

--- src/Posts.js
+++ src/Posts.js
@@ -3,7 +3,7 @@ import {VscBug} from 'react-icons/vsc'
 
 export const Posts = () => {
   return ( <button onClick={() => {
-    fetch('https://jsonplaceholder.typicodexyz.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error))
--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import { Posts } from "./Posts.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "Ryan ray ",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Cain Reales ",
+    image: "https://robohash.org/user3",
+  },
+  {
+    id: 3,
+    name: "Aleja Vivas ",
+    image: "https://robohash.org/user4",
+  },
+];
+
 // Hola mundo
-root.render(<>
-<Posts/>
-</>);
+root.render(
+  <>
+    {users.map((user, index) => {
+      return <div key= {index}>
+        <h1 >{user.name}  {index}</h1>
+        <img src={user.image}></img>
+      </div>;
+    })}
+  </>
+);
